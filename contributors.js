c5f204ab06fe92e47e7c604c9ad002d840677dfa
Sreejith M Varma <105853895+SreejithMVarma@users.noreply.github.com>
2023-10-06 00:13:46 +0530

    participants page

---
+++ contributors.js
@@ -0,0 +1,26 @@
+const contributors = [
+    {
+        imgSrc: 'images/Sreejith m varma.jpg',
+        title: 'Sreejith M Varma',
+        description: 'Hey Folks!👋🏻',
+        github: 'https://github.com/SreejithMVarma'
+    },
+    
+
+    // Add more contributors here as needed
+    /*
+    
+    COPY-PASTE BELOW TEMPLATE
+    {
+        imgSrc: 'images/YOUR FILE NAME',
+        title: 'YOUR NAME',
+        description: 'WHAT'S IN YOUR MIND',
+        github: 'https://github.com/your-github-username'
+    }
+    
+    
+    */
+];
+
+
+module.exports = contributors;
