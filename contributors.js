ce7b8a83c0950e7ba3c37251fea80b1459313af5
john-v-benny <120472886+john-v-benny@users.noreply.github.com>
2023-10-06 09:55:11 +0000

    good

--- contributors.js
+++ contributors.js
@@ -5,6 +5,13 @@ const contributors = [
         description: 'Hey Folks!👋🏻',
         github: 'https://github.com/SreejithMVarma'
     },
+
+    {
+        imgSrc: 'images/just chilling.jpg',
+        title: 'John Varghese Benny',
+        description: 'nokanda njan tharoola',
+        github: 'https://github.com/john-v-benny'
+    }
     
 
     // Add more contributors here as needed
