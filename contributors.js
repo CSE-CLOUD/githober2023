598041656e4b72aaa8b18c192cdbb5590f07557c
Theertha-santhosh <141830859+Theertha-santhosh@users.noreply.github.com>
2023-10-06 09:56:59 +0000

    Theertha

--- contributors.js
+++ contributors.js
@@ -5,17 +5,22 @@ const contributors = [
         description: 'Hey Folks!👋🏻',
         github: 'https://github.com/SreejithMVarma'
     },
-    
+    {
+        imgSrc: 'images/theertha.jpg',
+        title: 'Theertha santhosh',
+        description: 'hello..!!!',
+        github: 'https://github.com/Theertha-santhosh/githober2023.git'
+    }
 
     // Add more contributors here as needed
     /*
     
     COPY-PASTE BELOW TEMPLATE
     {
-        imgSrc: 'images/YOUR FILE NAME',
-        title: 'YOUR NAME',
-        description: 'WHAT'S IN YOUR MIND',
-        github: 'https://github.com/your-github-username'
+        imgSrc: '/home/student/image6.jpg',
+        title: 'THEERTHA SANTHOSH',
+        description: 'Currently studing in Nss college of engineering',
+        github: 'https://github.com/Theertha-santhosh/githober2023.git'
     }
     
     
